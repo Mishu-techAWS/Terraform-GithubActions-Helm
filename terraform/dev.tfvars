8fcd5eef63f99133c06943fdbb05f244a69fd7bc
Kishalay Bhattacharya <bhattacharyakishalay@gmail.com>
2024-11-25 19:18:01 +0530

    subnets categories modified

--- terraform/dev.tfvars
+++ terraform/dev.tfvars
@@ -2,4 +2,5 @@ project_id         = ""
 region             = "us-central1"
 vpc_name           = "devyodha-vpc"
 subnet_names       = ["private-subnet", "public-subnet"]
-subnet_cidr_ranges = ["10.0.0.0/16", "10.0.1.0/16"]
+public_subnet_cidr_ranges = ["10.0.0.0/28"]
+private_subnet_cidr_ranges = ["10.0.0.32/28"]
--- terraform/network.tf
+++ terraform/network.tf
@@ -4,17 +4,17 @@ resource "google_compute_network" "vpc" {
 }
 
 resource "google_compute_subnetwork" "private_subnet" {
-  name                     = "${var.region}-${var.subnet_names[0]}"
+  name                     = "${var.region}-${var.subnet_names[0]}-0"
   region                   = var.region
   network                  = google_compute_network.vpc.name
-  ip_cidr_range            = var.subnet_cidr_ranges[0]
+  ip_cidr_range            = var.private_subnet_cidr_ranges[0]
   private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "public_subnet" {
-  name                     = "${var.region}-${var.subnet_names[1]}"
+  name                     = "${var.region}-${var.subnet_names[1]}-0"
   region                   = var.region
   network                  = google_compute_network.vpc.name
-  ip_cidr_range            = var.subnet_cidr_ranges[1]
+  ip_cidr_range            = var.public_subnet_cidr_ranges[0]
   private_ip_google_access = false
 }
